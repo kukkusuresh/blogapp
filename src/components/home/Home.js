a88342a4c49fc2e9d4d90561ed0e33f29ee07202
kukkusuresh <kukkusuresh18@gmail.com>
2022-02-10 02:17:04 +0530

    first

---
+++ src/components/home/Home.js
@@ -0,0 +1,13 @@
+//import react from "react";
+import './Home.css';
+
+function Home(props){
+ return(
+     <div className='home'>
+         <h1 id='head'>Ariel's Blog Home </h1>
+         <p>Create a beautiful blog that fits your style. </p>
+     </div>
+ )
+} 
+export default Home;
+
