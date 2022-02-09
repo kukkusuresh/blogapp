3bec12d07f8a1821f5e28b89d9361da4cbb372c1
kukkusuresh <kukkusuresh18@gmail.com>
2022-02-10 02:33:42 +0530

    theme1

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -4,7 +4,7 @@ import './Home.css';
 function Home(props){
  return(
      <div className='home'>
-         <h1 id='head'>Ariel's Blog Home </h1>
+         <h1 id='head'>Ariel's Blog Home!</h1>
          <p>Create a beautiful blog that fits your style. </p>
      </div>
  )
